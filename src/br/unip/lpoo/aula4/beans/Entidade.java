9eaa9437cbc569b9c1ef868704ac9318f0243ec9
dirceu <dirceu.semighini@gmail.com>
2013-03-18 17:50:46 -0300

    Entidade

---
+++ src/br/unip/lpoo/aula4/beans/Entidade.java
@@ -0,0 +1,13 @@
+package br.unip.lpoo.aula4.beans;
+
+/**
+ * Interface responsavel por definir os metodos basicos
+ * de uma entidade
+ * @author dirceu
+ *
+ */
+public interface Entidade {
+
+	public int getId();
+	public void setId(int id);
+}
